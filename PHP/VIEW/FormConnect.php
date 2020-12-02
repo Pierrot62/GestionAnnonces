0cfd6b4125b765a74e521d03da194e97c03a466a
unknown <tancraftwork@gmail.com>
2020-12-02 16:24:11 +0100

    fichier connect

---
+++ PHP/VIEW/FormConnect.php
@@ -0,0 +1,18 @@
+<form action="Index.php?Page=actionConnect" method="post">
+
+<div>
+<label for="emailUtilisateur">Adresse E-mail</label>
+<input type="text" name="emailUtilisateur" required />
+</div>
+<div>
+<label for="mdpUtilisateur">Mot de passe</label>
+<input type="password" name="mdpUtilisateur" required />
+</div>
+<div>
+    <button type="submit">Accueil</button>
+    <button type="submit">Connexion</button>
+</div>
+<div>
+    
+</div>
+</form>
