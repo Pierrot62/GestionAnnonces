b377cee9dd59abf3f50be197d7f4653a710a609e
unknown <tancraftwork@gmail.com>
2020-12-04 11:11:18 +0100

    nav

--- PHP/VIEW/Accueil.php
+++ PHP/VIEW/Accueil.php
@@ -1,48 +1,61 @@
 
 <?php
-if(isset($_GET['view']))
-{
-    $view=$_GET['view'];
-    if(isset($_SESSION['utilisateur']))
-    {
-        $annonces=AnnoncesManager::getByUser($_SESSION['utilisateur']);
+
+// $categorie=CategoriesManager::findById($_POST['idCategorie']);
+
+// if ($categorie->getIdCategorie() != '4')
+// {
+//     $annonces=AnnoncesManager::getByCategorie($categorie);
+
+// }
+// else
+// {
+//     $annonces=AnnoncesManager::getList();
+
+// }
+
+if (isset($_GET['view'])) {
+    $view = $_GET['view'];
+    if (isset($_SESSION['utilisateur'])) {
+        $annonces = AnnoncesManager::getByUser($_SESSION['utilisateur']);
     }
-}
-else{
-    $annonces=AnnoncesManager::getList();
-}
-foreach ($annonces as $uneannonce)
-{
-    $vendeur=UtilisateursManager::findById($uneannonce->getIdUtilisateur());
-    echo'<article>
-    <div class="imgAnnonce"><img src="IMG/imgAnnonce/'.$uneannonce->getLienImg().'" alt="photo annonce"></div>
-    <div class="divXl">
-        <div class="infoAnnnonce">
-            <div>'.$uneannonce->getTitreAnnonce().'</div>
-            <div><a href="index.php?page=viewannonce&id='.$uneannonce->getIdAnnonce().'">Detail</a></div>
-        </div>
-        <div class=" infoAnnnonce">
-            <div>Prix :'.' '.$uneannonce->getPrixAnnonce().' € </div>
-            <div>Mis en ligne le :'.$uneannonce->getDatePublication().'</div>
-            <div>Par :'.' '.$vendeur->getNomUtilisateur().'</div>
-            <div>Statut :'.' '.($uneannonce->getStatutAnnonce()==1?"disponible":"vendu").'</div>
-        </div>
-    </div>';
-    if(isset($_GET['view']))
-    {   
-         echo'<div class="crud">
-          <div><a href="index.php?page=FormAnnonces&mode=update&id='.$uneannonce->getIdAnnonce().'">Modifier</a></div>
-          <div>
-            <a href="index.php?page=FormAnnonces&mode=delete&id='.$uneannonce->getIdAnnonce().'">Supprimer</a
-            >
-          </div>
-          <div><a href="index.php?page=FormAnnonces&mode=detail&id='.$uneannonce->getIdAnnonce().'">Detail</a></div>
-        </div>';
+} else {
+    $categorie = CategoriesManager::findById($_POST['idCategorie']);
+    if ($categorie->getIdCategorie() != '4') {
+        $annonces = AnnoncesManager::getByCategorie($categorie);
+    } else {
+        $annonces = AnnoncesManager::getList();
     }
-    echo'</article>';
-}
-if(isset($_GET['view']))
-{
-    echo'<button><a href="index.php?page=accueil">Retour</a></button>';
 }
 
+foreach ($annonces as $uneannonce) {
+    $vendeur = UtilisateursManager::findById($uneannonce->getIdUtilisateur());
+    echo '<article>
+    <div class="imgAnnonce"><img src="IMG/imgAnnonce/' . $uneannonce->getLienImg() . '" alt="photo annonce"></div>
+    <div class="divXl">
+        <div class="infoAnnnonce">
+            <div>' . $uneannonce->getTitreAnnonce() . '</div>
+            <div><a href="index.php?page=viewannonce&id=' . $uneannonce->getIdAnnonce() . '">Detail</a></div>
+        </div>
+        <div class=" infoAnnnonce">
+            <div>Prix :' . ' ' . $uneannonce->getPrixAnnonce() . ' € </div>
+            <div>Mis en ligne le :' . $uneannonce->getDatePublication() . '</div>
+            <div>Par :' . ' ' . $vendeur->getNomUtilisateur() . '</div>
+            <div>Statut :' . ' ' . ($uneannonce->getStatutAnnonce() == 1 ? "disponible" : "vendu") . '</div>
+        </div>
+    </div>';
+    if (isset($_GET['view'])) {
+        echo '<div class="crud">
+          <div><a href="index.php?page=FormAnnonces&mode=update&id=' . $uneannonce->getIdAnnonce() . '">Modifier</a></div>
+          <div>
+            <a href="index.php?page=FormAnnonces&mode=delete&id=' . $uneannonce->getIdAnnonce() . '">Supprimer</a
+            >
+          </div>
+          <div><a href="index.php?page=FormAnnonces&mode=detail&id=' . $uneannonce->getIdAnnonce() . '">Detail</a></div>
+        </div>';
+    }
+    echo '</article>';
+}
+if (isset($_GET['view'])) {
+    echo '<button><a href="index.php?page=accueil">Retour</a></button>';
+}
--- PHP/VIEW/AfficheCategorie.php
+++ PHP/VIEW/AfficheCategorie.php
@@ -2,7 +2,7 @@
 
 $categorie=CategoriesManager::findById($_POST['idCategorie']);
 var_dump($categorie);
-if ($categorie != '4')
+if ($categorie->getIdCategorie() != '4')
 {
     $annonces=AnnoncesManager::getByCategorie($categorie);
     var_dump($annonces);
@@ -12,27 +12,3 @@ else
     $annonces=AnnoncesManager::getList();
     var_dump($annonces);
 }
-
-
-
-
-// foreach ($annonces as $uneannonce)
-// {
-//     $vendeur=UtilisateursManager::findById($uneannonce->getIdUtilisateur());
-//     echo'<article>
-//     <div class="imgAnnonce"><img src="IMG/imgAnnonce/'.$uneannonce->getLienImg().'" alt="photo annonce"></div>
-//     <div class="divXl">
-//         <div class="infoAnnnonce">
-//             <div>'.$uneannonce->getTitreAnnonce().'</div>
-//             <div><a href="index.php?page=viewannonce&id='.$uneannonce->getIdAnnonce().'">Detail</a></div>
-//         </div>
-//         <div class=" infoAnnnonce">
-//             <div>Prix :'.' '.$uneannonce->getPrixAnnonce().' € </div>
-//             <div>Mis en ligne le :'.$uneannonce->getDatePublication().'</div>
-//             <div>Par :'.' '.$vendeur->getNomUtilisateur().'</div>
-//             <div>Statut :'.' '.($uneannonce->getStatutAnnonce()==1?"disponible":"vendu").'</div>
-//         </div>
-//     </div>';
-// }
-
-?>
