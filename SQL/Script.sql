c1888cc9dbd12f00e8533d26886e2cbd2b065fb0
bryanhovelynck <bryan.hovelynck@hotmail.com>
2020-12-04 09:19:01 +0100

    04/12/2020 (b)

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -52,6 +52,9 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `idRole` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+
+
 ALTER TABLE `annonces` ADD CONSTRAINT `FK_annonces_categories` FOREIGN KEY (`idCategorie`) REFERENCES `categories` (`idCategorie`);
 ALTER TABLE `annonces` ADD CONSTRAINT `FK_annonces_utilisateurs` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateurs` (`idUtilisateur`);
 
@@ -59,3 +62,4 @@ ALTER TABLE `messages` ADD CONSTRAINT `FK_messages_utilisateurs` FOREIGN KEY (`i
 
 
 ALTER TABLE `utilisateurs` ADD CONSTRAINT `FK_utilisateur_roles` FOREIGN KEY (`idRole`) REFERENCES `roles` (`idRole`);
+
